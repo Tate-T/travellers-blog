13cd5abe68a29fde5a7c5ca21bf1e57c5b25ceab
Kamikadze57 <143871458+Kamikadze57@users.noreply.github.com>
2025-09-21 12:05:16 +0300

    start with page
    add avatar.png for test
    edit structure and start with styles

--- src/pages/ProfilePage/ProfilePage.jsx
+++ src/pages/ProfilePage/ProfilePage.jsx
@@ -1,20 +1,34 @@
-import {Header} from "../../commponents/Header/Header.jsx"
-import {Footer} from "../../commponents/Footer/Footer.jsx"
-import { Container } from "../../commponents/Container/Container"
+import { Header } from "../../commponents/Header/Header.jsx";
+import { Footer } from "../../commponents/Footer/Footer.jsx";
+import { Container } from "../../commponents/Container/Container";
+import Avatar from "../../images/profile-page-avatar.png";
+import style from "./ProfilePage.module.css";
 
 export const ProfilePage = () => {
-    return <>
-    <Header/>
-    <main>
-        <section>
-            <Container>
-                <img src="" alt=""/>
-                <h2>Анастасія Олійник</h2>
-                <p>Люблю активні подорожі та дослідження нових місць. Ділюся практичними порадами та маршрутами для мандрівників.</p>
-            </Container>
+  return (
+    <>
+      <Header />
+      <main>
+        <section className={style.profile}>
+          <Container>
+            <div className={style["profile-info__box"]}>
+              <img src={Avatar} alt="avatar" />
+              <div className={style.info__text}>
+                <h2 className={style["profile-info__title"]}>Анастасія Олійник</h2>
+                <p className={style["profile-info__desc"]}>
+                  Люблю активні подорожі та дослідження нових місць. Ділюся практичними порадами та маршрутами для мандрівників.
+                </p>
+              </div>
+            </div>
+          </Container>
         </section>
-        <section></section>
-    </main>
-    <Footer/>
+        <section>
+          <Container>
+            <h1 className={style.stories__title}>Історії Мандрівника</h1>
+          </Container>
+        </section>
+      </main>
+      <Footer />
     </>
-}
+  );
+};
