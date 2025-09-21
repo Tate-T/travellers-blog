3917d8ba5d93474c70e2e4b0c08e631d7c6b5795
lordmax12122 <116733872+lordmax12122@users.noreply.github.com>
2025-09-21 12:00:17 +0300

    Update TravellersPage.jsx

--- src/pages/TravellersPage/TravellersPage.jsx
+++ src/pages/TravellersPage/TravellersPage.jsx
@@ -5,7 +5,85 @@ export const TravellersPage = () => {
   return (
     <>
       <Header />
-      <main></main>
+      <main>
+        <h2>
+          Мандрівники
+        </h2>
+        <ul>
+          <li>
+            <img src="" alt="Анастасія Олійник" />
+            <h3>Анастасія Олійник</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros</p>
+            <button>Переглянути профіль</button>
+          </li>
+          <li>
+            <img src="" alt="Анастасія Олійник" />
+            <h3>Анастасія Олійник</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros</p>
+            <button>Переглянути профіль</button>
+          </li>
+          <li>
+            <img src="" alt="Анастасія Олійник" />
+            <h3>Анастасія Олійник</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros</p>
+            <button>Переглянути профіль</button>
+          </li>
+          <li>
+            <img src="" alt="Анастасія Олійник" />
+            <h3>Анастасія Олійник</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros</p>
+            <button>Переглянути профіль</button>
+          </li>
+          <li>
+            <img src="" alt="Анастасія Олійник" />
+            <h3>Анастасія Олійник</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros</p>
+            <button>Переглянути профіль</button>
+          </li>
+          <li>
+            <img src="" alt="Анастасія Олійник" />
+            <h3>Анастасія Олійник</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros</p>
+            <button>Переглянути профіль</button>
+          </li>
+          <li>
+            <img src="" alt="Анастасія Олійник" />
+            <h3>Анастасія Олійник</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros</p>
+            <button>Переглянути профіль</button>
+          </li>
+          <li>
+            <img src="" alt="Анастасія Олійник" />
+            <h3>Анастасія Олійник</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros</p>
+            <button>Переглянути профіль</button>
+          </li>
+          <li>
+            <img src="" alt="Анастасія Олійник" />
+            <h3>Анастасія Олійник</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros</p>
+            <button>Переглянути профіль</button>
+          </li>
+          <li>
+            <img src="" alt="Анастасія Олійник" />
+            <h3>Анастасія Олійник</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros</p>
+            <button>Переглянути профіль</button>
+          </li>
+          <li>
+            <img src="" alt="Анастасія Олійник" />
+            <h3>Анастасія Олійник</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros</p>
+            <button>Переглянути профіль</button>
+          </li>
+          <li>
+            <img src="" alt="Анастасія Олійник" />
+            <h3>Анастасія Олійник</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros</p>
+            <button>Переглянути профіль</button>
+          </li>
+        </ul>
+      </main>
       <Footer />
     </>
   );
