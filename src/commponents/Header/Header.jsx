971ec33825eda81902be6f325d5efa97207a8f60
vladyslava-martyniuk <vlada.martyniuk@gmail.com>
2025-09-28 09:31:12 +0300

    add classes

--- src/commponents/Header/Header.jsx
+++ src/commponents/Header/Header.jsx
@@ -4,14 +4,15 @@ export const Header = () => {
   return (
     <>
       <Container>
-        <nav>
-          <li><p><a href="../../HomePage/HomePage.jsx">Головна</a></p></li>
-          <li><p><a href="../../StoriesPage/StoriesPage.jsx">Авторизація</a></p></li>
-          <li><p><a href="../../TravellersPage/TravellersPage.jsx">Мандрівники</a></p></li>
+        <nav className="header__nav">
+          <ul className="header__list">
+          <li className="header__item"><p><a className="nav__links" href="../../HomePage/HomePage.jsx">Головна</a></p></li>
+          <li className="header__item"><p><a className="nav__links" href="../../StoriesPage/StoriesPage.jsx">Авторизація</a></p></li>
+          <li className="header__item"><p><a className="nav__links" href="../../TravellersPage/TravellersPage.jsx">Мандрівники</a></p></li></ul>
         </nav>
-        <div className="">
-          <button><a href="../../AuthPage/AuthPage.jsx">Вхід</a></button>
-           <button><a href="../../AuthPage/AuthPage.jsx">Реєстрація</a></button>
+        <div className="header__buttons">
+           <button className="header__button"><a className="header__button-link" href="../../AuthPage/AuthPage.jsx">Вхід</a></button>
+           <button className="header__button"><a className="header__button-link" href="../../AuthPage/AuthPage.jsx">Реєстрація</a></button>
         </div>
         </Container>
     </>
