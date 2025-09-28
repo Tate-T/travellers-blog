af7e93988762bc77d14af7f2010956e76504467c
vladyslava-martyniuk <vlada.martyniuk@gmail.com>
2025-09-28 15:50:29 +0300

    add styles to header

--- src/commponents/Header/Header.jsx
+++ src/commponents/Header/Header.jsx
@@ -1,20 +1,40 @@
 import { Container } from "../Container/Container";
 import styles from "./Header.module.css";
+import logo from "../../images/logo.svg"
+import burger from "../../images/burger.svg"
 export const Header = () => {
   return (
     <header className={styles.header}>
       <Container>
-
+        <div className={styles.header__main}>
+       <img src={logo} alt="" />
+       <p className={styles.header__text}>Подорожники</p>
+        </div>
         <nav className="header__nav">
-          <ul className="header__list">
-          <li className="header__item"><p><a className="nav__links" href="../../HomePage/HomePage.jsx">Головна</a></p></li>
-          <li className="header__item"><p><a className="nav__links" href="../../StoriesPage/StoriesPage.jsx">Авторизація</a></p></li>
-          <li className="header__item"><p><a className="nav__links" href="../../TravellersPage/TravellersPage.jsx">Мандрівники</a></p></li></ul>
+          <ul className={styles.header__list}>
+          <li className="header__item"><p><a className={styles.header__link} href="../../HomePage/HomePage.jsx">Головна</a></p></li>
+          <li className="header__item"><p><a className={styles.header__link} href="../../StoriesPage/StoriesPage.jsx">Історії</a></p></li>
+          <li className="header__item"><p><a className={styles.header__link} href="../../TravellersPage/TravellersPage.jsx">Мандрівники</a></p></li></ul>
         </nav>
-        <div className="header__buttons">
-           <button className="header__button"><a className="header__button-link" href="../../AuthPage/AuthPage.jsx">Вхід</a></button>
-           <button className="header__button"><a className="header__button-link" href="../../AuthPage/AuthPage.jsx">Реєстрація</a></button>
-
+        <div className={styles.header__buttons}>
+           <a className={styles.button__link} href="../../AuthPage/AuthPage.jsx">Вхід</a>
+           <a className={styles.button__link} href="../../AuthPage/AuthPage.jsx">Реєстрація</a>
+        </div>
+        <div className={styles.mobile}>
+          <a className={styles.mobile__link} href="../../AddStoryPage/AddStoryPage.jsx">Опублікувати історію</a>
+           <a href=""> <img src={burger} alt="" /> </a>
+        </div>
+        <div className={styles.container__div}>
+           <nav className="header__nav">
+          <ul className={styles.container__list}>
+          <li className="header__item"><p><a className={styles.container__link} href="../../HomePage/HomePage.jsx">Головна</a></p></li>
+          <li className="header__item"><p><a className={styles.container__link} href="../../StoriesPage/StoriesPage.jsx">Історії</a></p></li>
+          <li className="header__item"><p><a className={styles.container__link} href="../../TravellersPage/TravellersPage.jsx">Мандрівники</a></p></li></ul>
+        </nav>
+        <div className={styles.container__buttons}>
+           <a className={styles.container__link} href="../../AuthPage/AuthPage.jsx">Вхід</a>
+           <a className={styles.container__link} href="../../AuthPage/AuthPage.jsx">Реєстрація</a>
+        </div>
         </div>
         </Container>
         
