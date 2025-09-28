f9754bcce1ca31d5d55c5560b233b3fdde88184b
vladyslava-martyniuk <vlada.martyniuk@gmail.com>
2025-09-28 11:24:00 +0300

    part of headerstyles done

--- src/commponents/Header/Header.jsx
+++ src/commponents/Header/Header.jsx
@@ -1,19 +1,23 @@
 import { Container } from "../Container/Container";
-
+import styles from "./Header.module.css";
 export const Header = () => {
   return (
-    <>
+    <header className={styles.header}>
       <Container>
-        <nav>
-          <li><p><a href="../../HomePage/HomePage.jsx">Головна</a></p></li>
-          <li><p><a href="../../StoriesPage/StoriesPage.jsx">Авторизація</a></p></li>
-          <li><p><a href="../../TravellersPage/TravellersPage.jsx">Мандрівники</a></p></li>
+      
+        <nav className="header__nav">
+          <ul className={styles.header__list}>
+          <li className={styles.header__item}><p><a className={styles.header__link} href="../../HomePage/HomePage.jsx">Головна</a></p></li>
+          <li className={styles.header__item}><p><a className={styles.header__link} href="../../StoriesPage/StoriesPage.jsx">Авторизація</a></p></li>
+          <li className={styles.header__item}><p><a className={styles.header__link} href="../../TravellersPage/TravellersPage.jsx">Мандрівники</a></p></li>
+          </ul>
         </nav>
-        <div className="">
-          <button><a href="../../AuthPage/AuthPage.jsx">Вхід</a></button>
-           <button><a href="../../AuthPage/AuthPage.jsx">Реєстрація</a></button>
+        <div className={styles.header__buttons}>
+         <a  className={styles.button__link}href="../../AuthPage/AuthPage.jsx">Вхід</a>
+          <a className={styles.button__link}href="../../AuthPage/AuthPage.jsx">Реєстрація</a>
         </div>
         </Container>
-    </>
+        
+    </header>
   );
 };
