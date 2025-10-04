cd986234e079f403891d0fc8b563fcb36aaaa57e
Kamikadze57 <savickijpavel42@gmail.com>
2025-10-04 20:25:19 +0300

    MessageNoStories+ProfilePage V1.1
    Add files to jsx + style
    edit index.css for btn+a (font-family: inherit;)

--- src/pages/ProfilePage/ProfilePage.jsx
+++ src/pages/ProfilePage/ProfilePage.jsx
@@ -1,6 +1,7 @@
 import { Header } from "../../commponents/Header/Header.jsx";
 import { Footer } from "../../commponents/Footer/Footer.jsx";
 import { Container } from "../../commponents/Container/Container";
+import { MessageNoStories } from "../../commponents/MessageNoStories/MessageNoStories.jsx";
 import Avatar from "../../images/profile-page-avatar.png";
 import style from "./ProfilePage.module.css";
 
@@ -14,7 +15,7 @@ export const ProfilePage = () => {
             <div className={style["profile-info__box"]}>
               <img src={Avatar} alt="avatar" />
               <div className={style.info__text}>
-                <h2 className={style["profile-info__title"]}>Анастасія Олійник</h2>
+                <h2 className={style["profile-info__name"]}>Анастасія Олійник</h2>
                 <p className={style["profile-info__desc"]}>
                   Люблю активні подорожі та дослідження нових місць. Ділюся практичними порадами та маршрутами для мандрівників.
                 </p>
@@ -25,6 +26,7 @@ export const ProfilePage = () => {
         <section>
           <Container>
             <h1 className={style.stories__title}>Історії Мандрівника</h1>
+            {0 ? (<ul className={style.stories_list}></ul>) : (<MessageNoStories/>) }
           </Container>
         </section>
       </main>
